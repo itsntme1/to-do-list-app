5320766a5e2d3eefc8c438cc84fca0177e22a2bb
Jan Šparlinek <itisntme12@protonmail.com>
2025-05-10 21:28:06 +0200

    made a basic site structure

--- app.py
+++ app.py
@@ -12,5 +12,15 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+
+@app.route("/history")
+def history():
+    return render_template("history.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
