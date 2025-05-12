cfca2d7973c83bb07478e209c89b4e8da365a6bb
Jan Šparlinek <itisntme12@protonmail.com>
2025-05-12 20:29:55 +0200

    completed navbar

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 # See the LICENSE file in the root of the repository for details.
 
 from flask import Flask, render_template, request, redirect
+from flask_session import Session
 
 
 app = Flask(__name__)
@@ -9,7 +10,16 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    # if not logged in render landing page
+    if True:
+        return render_template("landing.html")
+
     return render_template("index.html")
+
+
+@app.route("/landing")
+def landing():
+    return render_template("landing.html")
 
 
 @app.route("/dashboard")
@@ -20,6 +30,16 @@ def dashboard():
 @app.route("/history")
 def history():
     return render_template("history.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
