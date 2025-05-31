060f7b10719eba45cde6569a10594ae7ea088304
Jan Šparlinek <itisntme12@protonmail.com>
2025-05-31 14:57:48 +0200

    added protection against users maliciously accesing wrong adresses; index.html: changed the indentation

---
+++ helpers.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+from flask import session, redirect
+from functools import wraps
+
+
+def login_required(func):
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if not session.get('user_id'):
+            return redirect('/login')
+        
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+def plot_pie(data: list[int], labels: list[str]):
+    plt.pie(data, labels=labels)
+
+    plt.show()
